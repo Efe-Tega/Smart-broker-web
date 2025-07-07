e16d754ccb5d303abf9787d78f3ff1aaa51615b8
EFe Tega <efetegaakpor@gmail>
2025-07-07 12:16:06 +0100

    add error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -20,5 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (QueryException $e, Request $request) {
+            return response()->view('errors.unexpected-value', ['errorMessage' => $e->getMessage()], 500);
+        });
     })->create();
